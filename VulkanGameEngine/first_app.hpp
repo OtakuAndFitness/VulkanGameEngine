c53d35686b9178b8f15fbf40365cd7dafc3989fe
Bowen Tao <bowentao2015@gmail.com>
2022-03-26 15:58:31 +0800

    2nd commit

--- VulkanGameEngine/first_app.hpp
+++ VulkanGameEngine/first_app.hpp
@@ -2,6 +2,10 @@
 
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
+#include "lve_device.hpp"
+#include "LveSwapChain.hpp"
+
+#include <memory>
 
 namespace lve {
 	class FirstApp
@@ -10,10 +14,31 @@ namespace lve {
 			static constexpr int WIDTH = 1920;
 			static constexpr int HEIGHT = 1080;
 
+			FirstApp();
+			~FirstApp();
+
+			FirstApp(const FirstApp &) = delete;
+			FirstApp &operator=(const FirstApp &) = delete;
+
 			void run();
 
 		private:
+			void createPipelineLayout();
+			void createPipeline();
+			void createCommandBuffers();
+			void drawFrame();
+
 			LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
-			LvePipeline lvePipeline{ "simple_shader.vert.spv", "simple_shader.frag.spv" };
+			LveDevice lveDevice{ lveWindow };
+			LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+			/*LvePipeline lvePipeline{
+				"simple_shader.vert.spv",
+				"simple_shader.frag.spv",
+				lveDevice,
+				LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)
+			};*/
+			std::unique_ptr<LvePipeline> lvePipeline;
+			VkPipelineLayout pipelineLayout;
+			std::vector<VkCommandBuffer> commandBuffers;
 	};
 }
--- VulkanGameEngine/lve_pipeline.hpp
+++ VulkanGameEngine/lve_pipeline.hpp
@@ -5,13 +5,58 @@
 #include <string>
 
 namespace lve {
+	
+	struct PipelineConfigInfo {
+		//PipelineConfigInfo() = default;
+		PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+		PipelineConfigInfo& operator = (const PipelineConfigInfo &) = delete;
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+
+	};
+
 	class LvePipeline {
 		public:
-			LvePipeline(const std::string& vertFilepath, const std::string& fragFilePath);
+			LvePipeline(
+				const std::string& vertFilepath, 
+				const std::string& fragFilePath, 
+				LveDevice &device, 
+				const PipelineConfigInfo &configInfo 
+			);
+
+			~LvePipeline();
+
+			LvePipeline(const LvePipeline&) = delete;
+			LvePipeline& operator=(const LvePipeline&) = delete;
+
+			void bind(VkCommandBuffer commandBuffer);
+
+			static void defaultPipelineConfigInfo(PipelineConfigInfo &configInfo, uint32_t width, uint32_t height);
 
 		private:
 			static std::vector<char> readFile(const std::string& filepath);
 
-			void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilePath);
+			void createGraphicsPipeline(
+				const std::string& vertFilepath, 
+				const std::string& fragFilePath,
+				const PipelineConfigInfo& configInfo
+			);
+
+			void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+			LveDevice& lveDevice;
+			VkPipeline graphicsPipeline;
+			VkShaderModule vertShaderModule;
+			VkShaderModule fragShaderMoule;
 	};
 }
